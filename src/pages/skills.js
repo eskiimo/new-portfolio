5c72dd88e59376018a1d6718ca7de023cedf4f19
Kareem Kamal <34511976+eskiimo@users.noreply.github.com>
2021-12-19 00:26:14 +0200

    Again

--- src/pages/skills.js
+++ src/pages/skills.js
@@ -7,7 +7,7 @@ const Skills = () => {
  return (
   <Container className="skills-container">
    <Row className="big-row">
-    <Col md="12">
+    <Col xs="11" md="6">
      <div>
       <p className="headline">Skills & Experience</p>
       <p className="skills-paragraph">
