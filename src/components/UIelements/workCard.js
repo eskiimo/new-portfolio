fbc67fa37779b27841aefa072a4170dc857d3620
eskiimo <kareeemgm@gmail.com>
2021-12-19 23:36:06 +0200

    LSV

--- src/components/UIelements/workCard.js
+++ src/components/UIelements/workCard.js
@@ -1,16 +1,31 @@
 import React from 'react';
-
+import { Row, Col } from 'reactstrap';
 import './workCard.css';
 
 const WorkCard = (props) => {
  return (
-  <div className={`work-card ${props.className}`} style={props.style}>
-   <img
-    src={props.link}
-    alt={props.alt}
-    style={{ width: props.width, height: props.width }}
-   />
-  </div>
+  <Row className={`work-row ${!props.shown && 'main-content-hide'}`}>
+   <button onClick={props.hide} className="close-button">
+    X
+   </button>
+   <Col sm="6" className="work-image-column">
+    <Row className="align-items-center">
+     <Col>
+      {' '}
+      <div className="work-image"></div>{' '}
+     </Col>
+    </Row>
+   </Col>
+   <Col className="work-info-paragraph" sm="6">
+    <div>
+     <p className="headline">Harth</p>
+     <p className="harth-paragraph">
+      {props.p1} <br />
+      {props.p2}
+     </p>
+    </div>
+   </Col>
+  </Row>
  );
 };
 
--- src/pages/workPage.js
+++ src/pages/workPage.js
@@ -1,68 +1,57 @@
-import React from 'react';
-import { Container, Row, Col } from 'reactstrap';
+import React, { useState } from 'react';
+import { Row, Col } from 'reactstrap';
+import WorkCard from '../components/UIelements/workCard';
 // import NewCarousel from '../components/UIelements/carousel';
+// import Modal from '../components/UIelements/Modal';
+// import Buton from '../components/FormElements/Button';
 import './workPage.css';
 
 const Work = () => {
- //  const [list, setList] = useState([
- //   '/assets/work/harth-1.jpg',
- //   '/assets/work/harth-2.jpg',
- //   '/assets/work/harth-3.jpg',
- //  ]);
- //  const [index, setIndex] = useState(0);
- //  const next = () => {
- //   console.log('next');
- //   if (index === list.length - 1) {
- //    setIndex(0);
- //   } else {
- //    setIndex(index + 1);
- //   }
- //  };
- //  const prev = () => {
- //   console.log('prev');
- //   if (index === 0) {
- //    setIndex(list.length - 1);
- //   } else {
- //    setIndex(index - 1);
- //   }
- //  };
+ const PROJECT_LIST = [
+  {
+   name: 'harth',
+   images: [
+    {
+     src: '',
+     alt: '',
+    },
+   ],
+   p1: ' Harith application The fastest way to buy and sell crops, farm equipment  and everything related to agriculture and farmers You can display products near you and choose the categories that suit your needs Please have apleasant experience!',
+   p2: 'Built the Front End with Flutter -cross platform framework- so the application is available on Google Play Store and Apple Store',
+  },
+ ];
+ const [isOpen, setisOpen] = useState(false);
+ const showModal = () => {
+  setisOpen(true);
+ };
+ const hideModal = () => {
+  setisOpen(false);
+ };
+
  return (
-  <Container className="work-container">
-   <Row className="work-row">
-    <Col sm="6" className="work-image-column">
-     {/* <NewCarousel /> */}
-     <Row className="align-items-center">
-      {/* <Col>
-       {' '}
-       <button onClick={prev}>L</button>{' '}
-      </Col> */}
-      <Col>
-       {' '}
-       <div className="work-image"></div>{' '}
-      </Col>
-      {/* <Col>
-       <button onClick={next}>R</button>
-      </Col> */}
-     </Row>
+  <Col className="work-container">
+   <Row className="cards-row">
+    <Col xs="12" sm="6" md="4" lg="3">
+     <div onClick={showModal} className="project-card">
+      <img src="./assets/jslogo.png" alt="harth logo" />
+     </div>
     </Col>
-    <Col className="work-info-paragraph" sm="6">
-     <div>
-      <p className="headline">Harth</p>
-      <p className="harth-paragraph">
-       Harith application The fastest way to buy and sell crops, farm equipment
-       and everything related to agriculture and farmers You can display
-       products near you and choose the categories that suit your needs Please
-       have a pleasant experience!
-      </p>
-      <p className="harth-paragraph">
-       Built the Front End with Flutter -cross platform framework- so the
-       application is available on Google Play Store and Apple Store
-      </p>
+    <Col xs="12" sm="6" md="4" lg="3">
+     <div onClick={showModal} className="project-card">
+      <img src="./assets/work/harth-logo.jpg" alt="harth logo" />
      </div>
     </Col>
    </Row>
+   <WorkCard
+    hide={hideModal}
+    show={showModal}
+    shown={isOpen}
+    p1={PROJECT_LIST[0].p1}
+    p2={PROJECT_LIST[0].p2}
+   />
+
    <br />
-  </Container>
+  </Col>
  );
 };
 export default Work;
