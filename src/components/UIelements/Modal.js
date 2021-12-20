0676c8afc3ecc0a209bc65679b327912ce786a26
eskiimo <kareeemgm@gmail.com>
2021-12-20 21:00:43 +0200

    modal inhanced

--- src/components/UIelements/Modal.js
+++ src/components/UIelements/Modal.js
@@ -27,23 +27,52 @@ const MyModal = (props) => {
      <Row className={`work-row ${!props.shown && 'main-content-hide'}`}>
       <Col md="6" className="work-image-column">
        <Row className="align-items-center">
-        <Col>
-         {' '}
-         <div className="work-image">
-          <img src={props.project.image} alt={props.project.name} />
-         </div>{' '}
+        <Col className="justify-content-center">
+         <Row>
+          <div className="work-image">
+           <img src={props.project.image} alt={props.project.name} />
+          </div>
+         </Row>
+         <Row className="justify-content-center">
+          <Col xs="12" md="8" className=" link  justify-content-center">
+           <button className="link link-google">
+            <a
+             style={{ display: 'block' }}
+             href={props.project.googleLink}
+             target="_blank"
+             rel="noreferrer"
+            >
+             .
+            </a>
+           </button>
+          </Col>
+          <Col xs="12" md="5" className=" link  justify-content-center">
+           <button className="link link-apple">
+            <a
+             style={{ display: 'block' }}
+             href={props.project.appleLink}
+             target="_blank"
+             rel="noreferrer"
+            >
+             .
+            </a>
+           </button>
+          </Col>
+         </Row>
         </Col>
        </Row>
       </Col>
       <Col md="6">
-       <p className="harth-paragraph"> {props.project.p1} </p>
-       <br />
-       <p className="harth-paragraph"> {props.project.p2} </p>
+       <div className="harth-paragraph">
+        <p> {props.project.p1} </p>
+        <br />
+        <p> {props.project.p2} </p>
+       </div>
       </Col>
      </Row>
     </ModalBody>
     <ModalFooter>
-     <Buton text="Cancel" onClick={props.hide} />
+     <Buton text="Close" onClick={props.hide} />
     </ModalFooter>
    </Modal>
   </React.Fragment>
--- src/pages/workPage.js
+++ src/pages/workPage.js
@@ -2,26 +2,18 @@ import React, { useState } from 'react';
 import { Row, Col } from 'reactstrap';
 import MyModal from '../components/UIelements/Modal';
 import './workPage.css';
+import logo from './logos/harth-logo.jpg';
+import pic from './logos/harth-1.jpg';
 const PROJECT_LIST = [
  {
-  logo:
-   'https://scontent.fcai1-2.fna.fbcdn.net/v/t1.6435-9/121069964_3496817150394950_6016076511336555736_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=174925&_nc_eui2=AeG-S3TysnNSsUySVfk6Mtpcru0hatbO83Ou7SFq1s7zc5cnnpjD0a8gS8PTsI_BAFnFoF8xFQw-kRzcO_-BfbNA&_nc_ohc=HNXzGo5UiA0AX_cjr58&_nc_ht=scontent.fcai1-2.fna&oh=00_AT8wIv-u3W5_a-KfOjyTtcZDzD1ZbM00NJd5NmYzW0CHuQ&oe=61E63EF1',
+  logo: logo,
   name: 'harth',
-  image:
-   'https://scontent.fcai1-2.fna.fbcdn.net/v/t1.6435-9/121069964_3496817150394950_6016076511336555736_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=174925&_nc_eui2=AeG-S3TysnNSsUySVfk6Mtpcru0hatbO83Ou7SFq1s7zc5cnnpjD0a8gS8PTsI_BAFnFoF8xFQw-kRzcO_-BfbNA&_nc_ohc=HNXzGo5UiA0AX_cjr58&_nc_ht=scontent.fcai1-2.fna&oh=00_AT8wIv-u3W5_a-KfOjyTtcZDzD1ZbM00NJd5NmYzW0CHuQ&oe=61E63EF1',
-
+  image: pic,
   p1: ' Harith application The fastest way to buy and sell crops, farm equipment  and everything related to agriculture and farmers You can display products near you and choose the categories that suit your needs Please have apleasant experience!',
   p2: 'Built the Front End with Flutter -cross platform framework- so the application is available on Google Play Store and Apple Store',
- },
- {
-  logo:
-   'https://scontent.fcai1-2.fna.fbcdn.net/v/t1.6435-9/120385343_3457347237675275_7997133935383516279_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=174925&_nc_eui2=AeE-6xHeiYKOVr_efVgMXZ5MzPvnGqcKVgbM--capwpWBjzmJNTpARkHCCr25fYwKUpvByi_pehO-5DLHOiCTHrj&_nc_ohc=MNBgP_f0yfQAX-PsKNP&tn=dXZ6-OeLT3y2veP3&_nc_ht=scontent.fcai1-2.fna&oh=00_AT96N0_irQwBuZXZJ3HC3jjs7c7louTBbstIO4C1HQT7Tg&oe=61E802F8',
-  name: 'de7k',
-  image:
-   'https://scontent.fcai1-2.fna.fbcdn.net/v/t1.6435-9/120385343_3457347237675275_7997133935383516279_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=174925&_nc_eui2=AeE-6xHeiYKOVr_efVgMXZ5MzPvnGqcKVgbM--capwpWBjzmJNTpARkHCCr25fYwKUpvByi_pehO-5DLHOiCTHrj&_nc_ohc=MNBgP_f0yfQAX-PsKNP&tn=dXZ6-OeLT3y2veP3&_nc_ht=scontent.fcai1-2.fna&oh=00_AT96N0_irQwBuZXZJ3HC3jjs7c7louTBbstIO4C1HQT7Tg&oe=61E802F8',
-
-  p1: ' Harith application The fastest way to buy and sell crops, farm equipment  and everything related to agriculture and farmers You can display products near you and choose the categories that suit your needs Please have apleasant experience!',
-  p2: 'Built the Front End with Flutter -cross platform framework- so the application is available on Google Play Store and Apple Store',
+  googleLink: 'https://play.google.com/store/apps/details?id=com.harth.store',
+  appleLink:
+   'https://apps.apple.com/eg/app/%D8%AD%D8%A7%D8%B1%D8%AB/id1553536945',
  },
 ];
 export const RenderProjectCards = (props) =>
@@ -34,7 +26,7 @@ export const RenderProjectCards = (props) =>
      }}
      className="project-card"
     >
-     <img src={project.image} alt={project.name} />
+     <img src={project.logo} alt={project.name} />
     </div>
    </Col>
   );
