5afbe5351394bcdc4e4145953c107432f28f6afd
eskiimo <kareeemgm@gmail.com>
2022-01-25 19:46:26 +0200

    text-shadow

--- src/pages/aboutPage.js
+++ src/pages/aboutPage.js
@@ -2,40 +2,46 @@ import React from 'react';
 import { Container, Row, Col } from 'reactstrap';
 import Avatar from '../components/UIelements/avatar';
 import './about.css';
+
 const About = () => {
- return (
-  <Container className="info-container">
-   <Col xs="12">
-    <Row>
-     <Col xs="8" sm="6">
-      {' '}
-      <Avatar
-       image="https://scontent.fcai1-2.fna.fbcdn.net/v/t1.6435-9/67758797_2399216700155006_4286278486965878784_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=174925&_nc_eui2=AeFlFZ-kzZwPZON7dHFywNLfZcv3kcNgTXRly_eRw2BNdNuxleh-KNWDE2R2I1AVcwDJTUcfb2k21tGTC-5aY_-2&_nc_ohc=sAsReJL_hIQAX_LL253&_nc_ht=scontent.fcai1-2.fna&oh=00_AT8JyntHO0MaoM1GkYdiI_c2W8QK-lNckkx4m7u0TUk5PA&oe=61E7DDB1"
-       alt="kareem kamal"
-       width="50%"
-       height="50%"
-      />
-     </Col>
-     <Col xs="8" md="6">
-      <div>
-       <p className="headline">About Me </p>
-       <p className="info-paragraph">Any Color You Like</p>
-       <p className="info-paragraph">
-        I'm a <span className="info-with-color">Front End Developer</span> based
-        in Egypt, Cairo.
-        <br />I have a serious passion for writing code, while I Listen to good
-        music Fan of <span className="info-with-color">Anjunadeep</span> and
-        LoFi beats. I like to share music and take kindda good pictures <br />I
-        can help build a nice atmosphere with people around me.
-        <br />
-        <br />
-        tbc..
-       </p>
-      </div>
-     </Col>
-    </Row>
-   </Col>
-  </Container>
- );
+   return (
+      <Container className="info-container">
+         <Col xs="12">
+            <Row>
+               <Col xs="8" sm="6">
+                  {' '}
+                  <Avatar
+                     image="https://scontent.fcai1-2.fna.fbcdn.net/v/t1.6435-9/67758797_2399216700155006_4286278486965878784_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=174925&_nc_eui2=AeFlFZ-kzZwPZON7dHFywNLfZcv3kcNgTXRly_eRw2BNdNuxleh-KNWDE2R2I1AVcwDJTUcfb2k21tGTC-5aY_-2&_nc_ohc=sAsReJL_hIQAX_LL253&_nc_ht=scontent.fcai1-2.fna&oh=00_AT8JyntHO0MaoM1GkYdiI_c2W8QK-lNckkx4m7u0TUk5PA&oe=61E7DDB1"
+                     alt="kareem kamal"
+                     width="50%"
+                     height="50%"
+                  />
+               </Col>
+               <Col xs="8" md="6">
+                  <div>
+                     <p className="headline">About Me </p>
+                     <p className="info-paragraph">Any Color You Like</p>
+                     <p className="info-paragraph">
+                        I'm a{' '}
+                        <span className="info-with-color">
+                           Front End Developer
+                        </span>{' '}
+                        based in Egypt, Cairo.
+                        <br />I have a serious passion for writing code, while I
+                        Listen to good music Fan of{' '}
+                        <span className="info-with-color">Anjunadeep</span> and
+                        LoFi beats. I like to share music and take kindda good
+                        pictures <br />I can help build a nice atmosphere with
+                        people around me.
+                        <br />
+                        <br />
+                        tbc..
+                     </p>
+                  </div>
+               </Col>
+            </Row>
+         </Col>
+      </Container>
+   );
 };
 export default About;
--- src/pages/contactPage.js
+++ src/pages/contactPage.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import {
- Form,
- FormGroup,
- Modal,
- ModalHeader,
- ModalBody,
- ModalFooter,
+   Form,
+   FormGroup,
+   Modal,
+   ModalHeader,
+   ModalBody,
+   ModalFooter,
 } from 'reactstrap';
 import Buton from '../components/FormElements/Button';
 import './contactPage.css';
@@ -13,132 +13,132 @@ import './contactPage.css';
 import CustomInput from '../components/FormElements/InputField';
 
 import {
- VALIDATOR_EMAIL,
- VALIDATOR_MINLENGTH,
- VALIDATOR_REQUIRE,
+   VALIDATOR_EMAIL,
+   VALIDATOR_MINLENGTH,
+   VALIDATOR_REQUIRE,
 } from '../components/util/validators';
 import Transition from '../components/UIelements/animation/Transition';
 import { useForm } from '../components/hooks/form-hook';
 
 const Contact = () => {
- const [bool, setBool] = useState(false);
- const [isToast, setIsToast] = useState(false);
- const [formState, inputHandler] = useForm(
-  {
-   name: { value: '', isValid: false },
-   email: { value: '', isValid: false },
-   message: { value: '', isValid: false },
-  },
-  false
- );
- const showToast = () => {
-  setIsToast(true);
- };
- const hideToast = () => {
-  setIsToast(false);
- };
+   const [bool, setBool] = useState(false);
+   const [isToast, setIsToast] = useState(false);
+   const [formState, inputHandler] = useForm(
+      {
+         name: { value: '', isValid: false },
+         email: { value: '', isValid: false },
+         message: { value: '', isValid: false },
+      },
+      false
+   );
+   const showToast = () => {
+      setIsToast(true);
+   };
+   const hideToast = () => {
+      setIsToast(false);
+   };
 
- useEffect(() => {
-  setBool(true);
- }, []);
- // const
- const onSubmitHandler = (event) => {
-  event.preventDefault();
-  console.log(formState.inputs);
-  showToast();
- };
+   useEffect(() => {
+      setBool(true);
+   }, []);
+   // const
+   const onSubmitHandler = (event) => {
+      event.preventDefault();
+      console.log(formState.inputs);
+      showToast();
+   };
 
- let h13 = ['C', 'o', 'n', ' t', 'a', 'c', 't', '', 'm', 'e'];
- const mapH3 = h13.map((letter, index) => {
-  return (
-   <div key={index} className="parent">
-    <span className="animate">{letter}</span>
-   </div>
-  );
- });
- return (
-  <React.Fragment>
-   <div className="contact-container">
-    <p className="opening-tag html-snippets">&lt;html &gt;</p>
-    <p className="html-snippets"> &lt;body &gt;</p>
-    <p className="html-snippets">&lt;h1 &gt;</p>
+   let h13 = ['C', 'o', 'n', ' t', 'a', 'c', 't', '', 'm', 'e'];
+   const mapH3 = h13.map((letter, index) => {
+      return (
+         <div key={index} className="parent">
+            <span className="animate">{letter}</span>
+         </div>
+      );
+   });
+   return (
+      <React.Fragment>
+         <div className="contact-container">
+            <p className="opening-tag html-snippets">&lt;html &gt;</p>
+            <p className="html-snippets"> &lt;body &gt;</p>
+            <p className="html-snippets">&lt;h1 &gt;</p>
 
-    <div className="contact-paragraph">{mapH3}</div>
-    <p className="html-snippets"> &lt;p &gt;</p>
-    <p className="contact-message">
-     I'm interested in freelance opportunities , especially ambitious or large
-     projects. However, if you have other request or question, don't hesitate to
-     use the form.
-    </p>
-    <p className="html-snippets"> &lt;form &gt;</p>
-    <Form className="form" onSubmit={onSubmitHandler}>
-     <Transition bool={bool}>
-      <FormGroup>
-       <CustomInput
-        id="name"
-        placeholder="Name"
-        type="text"
-        errorText="idiot!!"
-        validators={[VALIDATOR_REQUIRE()]}
-        onInput={inputHandler}
-       />
-      </FormGroup>
-     </Transition>
-     <Transition bool={bool}>
-      <FormGroup>
-       <CustomInput
-        id="email"
-        placeholder="Email"
-        type="email"
-        errorText="idiot!!"
-        validators={[VALIDATOR_EMAIL()]}
-        onInput={inputHandler}
-       />
-      </FormGroup>
-     </Transition>
+            <div className="contact-paragraph">{mapH3}</div>
+            <p className="html-snippets"> &lt;p &gt;</p>
+            <p className="contact-message">
+               I'm interested in freelance opportunities , especially ambitious
+               or large projects. However, if you have other request or
+               question, don't hesitate to use the form.
+            </p>
+            <p className="html-snippets"> &lt;form &gt;</p>
+            <Form className="form" onSubmit={onSubmitHandler}>
+               <Transition bool={bool}>
+                  <FormGroup>
+                     <CustomInput
+                        id="name"
+                        placeholder="Name"
+                        type="text"
+                        errorText="idiot!!"
+                        validators={[VALIDATOR_REQUIRE()]}
+                        onInput={inputHandler}
+                     />
+                  </FormGroup>
+               </Transition>
+               <Transition bool={bool}>
+                  <FormGroup>
+                     <CustomInput
+                        id="email"
+                        placeholder="Email"
+                        type="email"
+                        errorText="idiot!!"
+                        validators={[VALIDATOR_EMAIL()]}
+                        onInput={inputHandler}
+                     />
+                  </FormGroup>
+               </Transition>
 
-     <Transition bool={bool}>
-      <FormGroup>
-       <CustomInput
-        id="message"
-        placeholder="Message"
-        type="textarea"
-        errorText="idiot!!"
-        validators={[VALIDATOR_MINLENGTH(5)]}
-        onInput={inputHandler}
-       />
-      </FormGroup>
-     </Transition>
-     <div className="form-button">
-      <Buton
-       disabled={!formState.isValid}
-       type="submit"
-       width={'30%'}
-       text={'Submit'}
-      />
-     </div>
-    </Form>
-    <p className="html-snippets"> &lt;/body &gt;</p>
-   </div>
-   <Modal
-    backdrop={true}
-    centered
-    fullscreen="sm"
-    size="xs"
-    toggle={showToast}
-    isOpen={isToast}
-   >
-    <ModalHeader toggle={isToast}>
-     <i class="fas fa-kiss-wink-heart"></i>{' '}
-    </ModalHeader>
-    <ModalBody>
-     <p>thank you !</p>
-    </ModalBody>
-    <ModalFooter>
-     <Buton text="Close" onClick={hideToast} />
-    </ModalFooter>
-   </Modal>
-  </React.Fragment>
- );
+               <Transition bool={bool}>
+                  <FormGroup>
+                     <CustomInput
+                        id="message"
+                        placeholder="Message"
+                        type="textarea"
+                        errorText="idiot!!"
+                        validators={[VALIDATOR_MINLENGTH(5)]}
+                        onInput={inputHandler}
+                     />
+                  </FormGroup>
+               </Transition>
+               <div className="form-button">
+                  <Buton
+                     disabled={!formState.isValid}
+                     type="submit"
+                     width={'30%'}
+                     text={'Submit'}
+                  />
+               </div>
+            </Form>
+            <p className="html-snippets"> &lt;/body &gt;</p>
+         </div>
+         <Modal
+            backdrop={true}
+            centered
+            fullscreen="sm"
+            size="xs"
+            toggle={showToast}
+            isOpen={isToast}
+         >
+            <ModalHeader toggle={isToast}>
+               <i class="fas fa-kiss-wink-heart"></i>{' '}
+            </ModalHeader>
+            <ModalBody>
+               <p>thank you !</p>
+            </ModalBody>
+            <ModalFooter>
+               <Buton text="Close" onClick={hideToast} />
+            </ModalFooter>
+         </Modal>
+      </React.Fragment>
+   );
 };
 export default Contact;
