99ec0d87fc4d52d1b8e55a8e074d46671fa6ba9d
eskiimo <kareeemgm@gmail.com>
2022-07-25 19:47:23 +0200

    Any Color You Like

--- src/App.js
+++ src/App.js
@@ -11,40 +11,65 @@ import Work from './pages/workPage';
 import { HideContext } from './components/util/context';
 
 const App = () => {
- const [isHidden, setHidden] = useState(false);
- const showContent = useCallback(() => {
-  setHidden(false);
- }, []);
+   const [isHidden, setHidden] = useState(false);
+   const [play, setPlay] = useState(true);
+   const [playMode, setPlayMode] = useState('pause');
+   const toggleAudio = () => {
+      if (play) {
+         setPlay(false);
+         setPlayMode('pause');
+      } else {
+         setPlay(true);
+         setPlayMode('play');
+      }
+   };
+   const showContent = useCallback(() => {
+      setHidden(false);
+   }, []);
 
- const hideContent = useCallback(() => {
-  setHidden(true);
- }, []);
- let routes = (
-  <Routes>
-   <Route path="/" element={<Home />}></Route>
-   <Route path="/skills" element={<Skills />}></Route>
-   <Route path="/about" element={<About />}></Route>
-   <Route path="/work" element={<Work />}></Route>
-   <Route path="/contact" element={<Contact />}></Route>
-  </Routes>
- );
- return (
-  <HideContext.Provider
-   value={{
-    hidden: isHidden,
-    hide: hideContent,
-    show: showContent,
-   }}
-  >
-   <Router className="head">
-    <NavigationBar />
-    <main className={isHidden ? 'main-content-hide' : 'main-content'}>
-     {routes}
-    </main>
-    ){/* <NavigationBar /> */}
-   </Router>
-  </HideContext.Provider>
- );
+   const hideContent = useCallback(() => {
+      setHidden(true);
+   }, []);
+   let routes = (
+      <Routes>
+         <Route path="/" element={<Home />}></Route>
+         <Route path="/skills" element={<Skills />}></Route>
+         <Route path="/about" element={<About />}></Route>
+         <Route path="/work" element={<Work />}></Route>
+         <Route path="/contact" element={<Contact />}></Route>
+      </Routes>
+   );
+   return (
+      <HideContext.Provider
+         value={{
+            hidden: isHidden,
+            hide: hideContent,
+            show: showContent,
+         }}
+      >
+         <Router className="head">
+            <NavigationBar />
+            <main className={isHidden ? 'main-content-hide' : 'main-content'}>
+               {play ? (
+                  <audio autoPlay controls className="record spin">
+                     <source
+                        src="/assets/songs/AnyColourYouLike.mp3"
+                        type="audio/mpeg"
+                     />
+                  </audio>
+               ) : (
+                  <></>
+               )}
+               <div className={`${'record-cover-' + playMode}`}>
+                  <button onClick={toggleAudio} className="record-btn"></button>
+               </div>
+
+               {routes}
+            </main>
+            {/* <NavigationBar /> */}
+         </Router>
+      </HideContext.Provider>
+   );
 };
 
 export default App;
