fa606e5399fabea487821db05e6c3b1763cabecc
eskiimo <kareeemgm@gmail.com>
2022-08-28 23:21:08 +0200

    w

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 // import NavigationBar from './components/Navigation/NavigationBar';
 import About from './pages/aboutPage';
