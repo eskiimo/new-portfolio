4f086b5e8e3e98d0e902d9a6eb0dc7cadff01b66
eskiimo <kareeemgm@gmail.com>
2023-01-21 20:43:25 +0200

    added eps to work list

--- src/pages/aboutPage.js
+++ src/pages/aboutPage.js
@@ -1,43 +1,41 @@
-import React from 'react';
-import { Container, Row, Col } from 'reactstrap';
-import Avatar from '../components/UIelements/avatar';
-import me from './logos/me.jpg';
-import './about.css';
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import Avatar from "../components/UIelements/avatar";
+import "./about.css";
 
 const About = () => {
-   return (
-      <Container className="info-container align-content-center">
-         <Col xs="12">
-            <Row>
-               <Col xs="8" lg="3">
-                  {' '}
-                  <Avatar image={me} alt="kareem kamal" width="80%" />
-               </Col>
-               <Col xs="8" lg="9">
-                  <div>
-                     <div className="dang">
-                        <h1 data-shadow="dang!">Developer</h1>
-                     </div>
-                     <p className="info-paragraph">
-                        I'm a{' '}
-                        <span className="info-with-color">
-                           Front End Developer
-                        </span>{' '}
-                        based in Egypt, Cairo. Studied Engineering at University
-                        of Tanta.
-                        <br />I have a serious passion for writing code, while I
-                        Listen to good music.
-                        <br /> Fan of{' '}
-                        <span className="info-with-color">React.js</span> and
-                        Web dev. in general. on my way to be a{' '}
-                        <span className="info-with-color">MERN</span> dev....
-                        <br />
-                     </p>
-                  </div>
-               </Col>
-            </Row>
-         </Col>
-      </Container>
-   );
+  return (
+    <Container className="info-container align-content-center">
+      <Col xs="12">
+        <Row>
+          <Col xs="8" lg="3">
+            {" "}
+            <Avatar image="/assets/me.jpg" alt="kareem kamal" width="80%" />
+          </Col>
+          <Col xs="8" lg="9">
+            <div>
+              <div className="dang">
+                <h1 data-shadow="dang!">Developer</h1>
+              </div>
+              <p className="info-paragraph">
+                I'm a{" "}
+                <span className="info-with-color">Front End Developer</span>{" "}
+                based in Egypt, Cairo. Studied Engineering at University of
+                Tanta.
+                <br />I have a serious passion for writing code, while I Listen
+                to good music.
+                <br /> Fan of <span className="info-with-color">
+                  React.js
+                </span>{" "}
+                and Web dev. in general. on my way to be a{" "}
+                <span className="info-with-color">MERN</span> dev....
+                <br />
+              </p>
+            </div>
+          </Col>
+        </Row>
+      </Col>
+    </Container>
+  );
 };
 export default About;
--- src/pages/workPage.js
+++ src/pages/workPage.js
@@ -1,83 +1,83 @@
-import React, { useState } from 'react';
-import { Row, Col } from 'reactstrap';
-import MyModal from '../components/UIelements/Modal';
-import './workPage.css';
-import logo from './logos/harth-logo.jpg';
-import pic from './logos/harth-1.jpg';
-import acadLogo from './logos/acad-mern.jpg';
-import acadHome from './logos/acad-mern-home.jpg';
+import React, { useState } from "react";
+import { Row, Col } from "reactstrap";
+import MyModal from "../components/UIelements/Modal";
+import "./workPage.css";
 
 const PROJECT_LIST = [
-   {
-      logo: logo,
-      name: 'Harth - حارث ',
-      image: pic,
-      p1: ' Harith application The fastest way to buy and sell crops, farm equipment  and everything related to agriculture and farmers You can display products near you and choose the categories that suit your needs Please have apleasant experience!',
-      p2: 'Built the Front End with Flutter -cross platform framework- so the application is available on Google Play Store and Apple Store',
-      googleLink:
-         'https://play.google.com/store/apps/details?id=com.harth.store',
-      appleLink:
-         'https://apps.apple.com/eg/app/%D8%AD%D8%A7%D8%B1%D8%AB/id1553536945',
-   },
-   {
-      logo: acadLogo,
-      name: 'MERN',
-      image: acadHome,
-      p1: ' React, NodeJS, Express & MongoDB - The MERN Fullstack Guide',
-      p2: 'Created by Academind by Maximilian Schwarzmüller, Maximilian Schwarzmüller, Manuel Lorenz',
-      link: 'https://acad-mern-kareem.netlify.app/',
-   },
+  {
+    logo: "/assets/harth-logo.jpg",
+    name: "Harth - حارث ",
+    image: "/assets/harth-1.jpg",
+    p1: " Harith application The fastest way to buy and sell crops, farm equipment  and everything related to agriculture and farmers You can display products near you and choose the categories that suit your needs Please have apleasant experience!",
+    p2: "Built the Front End with Flutter -cross platform framework- so the application is available on Google Play Store and Apple Store",
+    googleLink: "https://play.google.com/store/apps/details?id=com.harth.store",
+    appleLink:
+      "https://apps.apple.com/eg/app/%D8%AD%D8%A7%D8%B1%D8%AB/id1553536945",
+  },
+  {
+    logo: "/assets/acad-mern.jpg",
+    name: "MERN",
+    image: "/assets/acad-mern-home.jpg",
+    p1: " React, NodeJS, Express & MongoDB - The MERN Fullstack Guide",
+    p2: "Created by Academind by Maximilian Schwarzmüller, Maximilian Schwarzmüller, Manuel Lorenz",
+    link: "https://acad-mern-kareem.netlify.app/",
+  },
+  {
+    logo: "/assets/epsLogo.png",
+    name: "EPS",
+    image: "/assets/epsHome.JPG",
+    p1: " Landing page for an electric services company",
+    p2: "EPS provides All field technical services, tooling, equipment, instrumentation, and technical supervision to perform such tests and inspections.",
+    link: "https://eps-co.netlify.app",
+  },
 ];
 export const RenderProjectCards = (props) =>
-   props.list.map((project, index) => {
-      return (
-         <Col key={project.name} xs="11" sm="6" md="3" lg="2">
-            <div
-               onClick={() => {
-                  props.onClick(index);
-               }}
-               className="project-card ml-10"
-            >
-               <img src={project.logo} alt={project.name} />
-            </div>
-         </Col>
-      );
-   });
+  props.list.map((project, index) => {
+    return (
+      <Col key={project.name} xs="11" sm="6" md="3" lg="2">
+        <div
+          onClick={() => {
+            props.onClick(index);
+          }}
+          className="project-card ml-10"
+        >
+          <img src={project.logo} alt={project.name} />
+        </div>
+      </Col>
+    );
+  });
 
 const Work = () => {
-   const [isOpen, setisOpen] = useState(false);
+  const [isOpen, setisOpen] = useState(false);
 
-   const showModal = () => {
-      setisOpen(true);
-   };
-   const hideModal = () => {
-      setisOpen(false);
-   };
-   const [selectedProject, setSelProject] = useState(PROJECT_LIST[0]);
-   const handleOnClick = (index) => {
-      setSelProject(PROJECT_LIST[index]);
-      showModal();
-   };
-   return (
-      <>
-         <MyModal
-            project={selectedProject}
-            shown={isOpen}
-            hide={hideModal}
-            show={showModal}
-         />
+  const showModal = () => {
+    setisOpen(true);
+  };
+  const hideModal = () => {
+    setisOpen(false);
+  };
+  const [selectedProject, setSelProject] = useState(PROJECT_LIST[0]);
+  const handleOnClick = (index) => {
+    setSelProject(PROJECT_LIST[index]);
+    showModal();
+  };
+  return (
+    <>
+      <MyModal
+        project={selectedProject}
+        shown={isOpen}
+        hide={hideModal}
+        show={showModal}
+      />
 
-         <div className="work-container">
-            <hr />
-            <p>PROJECTS</p>
-            <Row className="cards-row">
-               <RenderProjectCards
-                  list={PROJECT_LIST}
-                  onClick={handleOnClick}
-               />
-            </Row>
-         </div>
-      </>
-   );
+      <div className="work-container">
+        <hr />
+        <p>PROJECTS</p>
+        <Row className="cards-row">
+          <RenderProjectCards list={PROJECT_LIST} onClick={handleOnClick} />
+        </Row>
+      </div>
+    </>
+  );
 };
 export default Work;
